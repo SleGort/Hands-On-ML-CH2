f9c64903b28dd9b706944ae3c3537be8b40b9c93
Alex <appwork1020@gmail.com>
2024-08-18 15:59:52 +0200

    added usefull paths to the config file.

--- src/config.py
+++ src/config.py
@@ -1,1 +1,16 @@
 # Store useful variables and configuration
+
+# ? File paths-data
+PATH_DATA_EXTERNAL = "/data/external"
+PATH_DATA_INTERIM = "/data/interim"
+PATH_DATA_PROCESSED = "/data/processed"
+PATH_DATA_RAW = "/data/raw"
+
+# ? File paths - modeling
+PATH_MODELING = "/src/modeling"
+
+# ? File paths - services
+PATH_SERVICES = "/src/services"
+
+# ? File paths - figures
+PATH_FIGURES = "/reports/figures"
